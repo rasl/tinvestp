bc1d5eafb3c8cb6c7fe8a38905ebfe46bd40673b
Dmitry Rassolov <dmitriy.rassolov@gmail.com>
2021-11-13 17:13:08 +0300

    feat: add simple reinstall shell script

---
+++ full_reinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. .env
+
+docker-compose down && rm -rf data/postgres
+docker-compose up -d
+RETRIES=5
+until docker-compose exec db psql --user $POSTGRES_USER -d postgres -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+    echo "Waiting postgres..."
+    sleep 1
+done
+
+cat db/create_schema.sql| docker-compose exec -T db psql --user $POSTGRES_USER -d $POSTGRES_DB
+cat db/test_data.sql| docker-compose exec -T db psql --user $POSTGRES_USER -d $POSTGRES_DB
