b5c39b17ac8e86ec2400ed94df5b35a48dc615a9
Dmitry Rassolov <dmitriy.rassolov@gmail.com>
2021-11-16 19:36:16 +0300

    chore: fix code style

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -97,11 +97,21 @@ CREATE TABLE "transaction" (
 
 CREATE VIEW "asset_history" AS (
   SELECT
-  transaction.uuid AS transaction_uuid, transaction.operation, event.type AS transaction_type, transaction.quantity,
+  transaction.uuid AS transaction_uuid,
+  transaction.operation,
+  event.type AS transaction_type,
+  transaction.quantity,
   account.uuid AS account_uuid,
-  asset.uuid AS asset_uuid, asset.ticker, asset.name, asset.description, asset.type AS asset_type,
-  exchange_rate.uuid AS exchange_rate_uuid, exchange_rate.exchange_rate_value, exchange_rate.datetime,
-  exchange_rate.asset_from_uuid, exchange_rate.asset_to_uuid
+  asset.uuid AS asset_uuid,
+  asset.ticker,
+  asset.name,
+  asset.description,
+  asset.type AS asset_type,
+  exchange_rate.uuid AS exchange_rate_uuid,
+  exchange_rate.exchange_rate_value,
+  exchange_rate.datetime,
+  exchange_rate.asset_from_uuid,
+  exchange_rate.asset_to_uuid
   FROM transaction
   LEFT JOIN event ON (transaction.event_uuid = event.uuid)
   LEFT JOIN account ON (transaction.account_uuid = account.uuid)
@@ -161,7 +171,9 @@ CREATE VIEW "asset_buy_sell_result" AS (
 
     FROM (
       SELECT
-      asset_uuid, asset_type, asset_name,
+      asset_uuid,
+      asset_type,
+      asset_name,
       account_uuid,
       SUM(CASE WHEN event_type = 'buy' THEN transaction_quantity END) AS buy_quantity,
       SUM(CASE WHEN event_type = 'sell' THEN transaction_quantity END) AS sell_quantity,
@@ -175,11 +187,19 @@ CREATE VIEW "asset_buy_sell_result" AS (
 
       FROM (
         SELECT
-        t.uuid AS transaction_uuid, t.operation AS transaction_operation, t.quantity AS transaction_quantity,
+        t.uuid AS transaction_uuid,
+        t.operation AS transaction_operation,
+        t.quantity AS transaction_quantity,
         e.type AS event_type,
         account.uuid AS account_uuid,
-        asset.uuid AS asset_uuid, asset.type AS asset_type, asset.name AS asset_name, asset.description AS asset_description,
-        erl.exchange_rate_value AS exchange_rate_value_current, erl.uuid AS exchange_rate_uuid, erl.asset_from_uuid AS exchange_rate_asset_from_uuid, erl.asset_to_uuid AS exchange_rate_asset_to_uuid,
+        asset.uuid AS asset_uuid,
+        asset.type AS asset_type,
+        asset.name AS asset_name,
+        asset.description AS asset_description,
+        erl.exchange_rate_value AS exchange_rate_value_current,
+        erl.uuid AS exchange_rate_uuid,
+        erl.asset_from_uuid AS exchange_rate_asset_from_uuid,
+        erl.asset_to_uuid AS exchange_rate_asset_to_uuid,
         er.exchange_rate_value AS exchange_rate_value
 
         FROM transaction t
@@ -187,7 +207,8 @@ CREATE VIEW "asset_buy_sell_result" AS (
         LEFT JOIN account ON (t.account_uuid = account.uuid)
         LEFT JOIN asset ON (asset.uuid = account.asset_uuid)
         LEFT JOIN asset_rub ON (1=1)
-        LEFT JOIN exchange_rate_last erl ON (erl.asset_from_uuid=account.asset_uuid AND erl.asset_to_uuid=asset_rub.uuid)
+        LEFT JOIN exchange_rate_last erl ON
+          (erl.asset_from_uuid=account.asset_uuid AND erl.asset_to_uuid=asset_rub.uuid)
         LEFT JOIN exchange_rate er ON (t.exchange_rate_uuid = er.uuid)
       ) quantity_aggregation
       GROUP BY asset_uuid, asset_type, asset_name, account_uuid, exchange_rate_value_current
@@ -210,7 +231,8 @@ CREATE VIEW "account_extra_result" AS (
 
   FROM (
     SELECT
-    e.source_account_uuid, array_agg(t.account_uuid) to_accounts,
+    e.source_account_uuid,
+    array_agg(t.account_uuid) to_accounts,
     SUM(CASE WHEN e.type = 'interest' THEN t.quantity END) AS interest_from_source_qnt,
     SUM(CASE WHEN e.type = 'dividends' THEN t.quantity END) AS dividends_from_source_qnt,
     SUM(CASE WHEN e.type = 'coupons' THEN t.quantity END) AS coupons_from_source_qnt,
@@ -231,15 +253,23 @@ CREATE VIEW "account_extra_result" AS (
 
 CREATE VIEW "asset_result" AS (
   SELECT
-  asset_uuid, asset_type, asset_name, account_uuid,
+  asset_uuid,
+  asset_type,
+  asset_name,
+  account_uuid,
   current_buy_sell_result + extra_result AS total_result,
   current_buy_sell_result + extra_result - spent_on_buy AS absolute_margin,
   ((current_buy_sell_result + extra_result) / spent_on_buy - 1) * 100 AS percent_margin,
   current_buy_sell_result,
   sell_result,
   extra_result,
-  buy_quantity, spent_on_buy, sell_quantity, earned_on_sell,
-  remain_quantity, exchange_rate_value_current, remain_value,
+  buy_quantity,
+  spent_on_buy,
+  sell_quantity,
+  earned_on_sell,
+  remain_quantity,
+  exchange_rate_value_current,
+  remain_value,
   extra_interest,
   extra_dividends,
   extra_coupons,
@@ -249,12 +279,20 @@ CREATE VIEW "asset_result" AS (
 
   FROM (
     SELECT
-    ar.asset_uuid, ar.asset_type, ar.asset_name, ar.account_uuid,
+    ar.asset_uuid,
+    ar.asset_type,
+    ar.asset_name,
+    ar.account_uuid,
     ar.total_value AS current_buy_sell_result,
     COALESCE(ar.absolute_margin, 0) AS sell_result,
     COALESCE(aer.total_result_qnt, 0) AS extra_result,
-    ar.buy_quantity, ar.spent_on_buy, ar.sell_quantity, ar.earned_on_sell,
-    ar.remain_quantity, ar.exchange_rate_value_current, ar.remain_value,
+    ar.buy_quantity,
+    ar.spent_on_buy,
+    ar.sell_quantity,
+    ar.earned_on_sell,
+    ar.remain_quantity,
+    ar.exchange_rate_value_current,
+    ar.remain_value,
     aer.interest_from_source_qnt AS extra_interest,
     aer.dividends_from_source_qnt AS extra_dividends,
     aer.coupons_from_source_qnt AS extra_coupons,
