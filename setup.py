f50e768db84ed2a793e6a3415aa821884015f1d4
Dmitry Rassolov <dmitriy.rassolov@gmail.com>
2021-11-24 11:05:51 +0300

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tinvestp',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/rasl/tinvestp',
+    license='MIT',
+    author='Dmitry Rassolov',
+    author_email='dmitriy.rassolov@gmail.com',
+    description='-'
+)
